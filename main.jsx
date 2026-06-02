390fa7f4ff556cbd5ecf60297498b6c04b50abf6
fiqrihnd <45643660+fiqrihnd@users.noreply.github.com>
2026-06-02 17:12:40 +0700

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css' // <-- INI WAJIB ADA
+import App from './App.jsx' // Mengimpor App.jsx dari root folder
+import './index.css'     // Mengimpor index.css dari root folder
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
