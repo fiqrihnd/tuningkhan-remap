769b7e96542956bfd9eafb3050db42444f513108
fiqrihnd <45643660+fiqrihnd@users.noreply.github.com>
2026-06-02 17:12:12 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: { extend: {} },
+  content: [
+    "./index.html",
+    "./App.jsx",
+    "./main.jsx",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./*.{js,ts,jsx,tsx}" // Memastikan semua file di root folder dipindai oleh Tailwind
+  ],
+  theme: {
+    extend: {},
+  },
   plugins: [],
 }
